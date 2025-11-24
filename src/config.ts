73090f6b3c0384b54ad86bb322420a532f1a783a
btop251 <3123913424@qq.com>
2025-11-24 09:50:20 +0800

    关闭了些没必要的功能

--- src/config.ts
+++ src/config.ts
@@ -40,8 +40,8 @@ export const siteConfig: SiteConfig = {
 		diary: true, // 日记页面开关
 		friends: true, // 友链页面开关
 		projects: false, // 项目页面开关
-		skills: true, // 技能页面开关
-		timeline: true, // 时间线页面开关
+		skills: false, // 技能页面开关
+		timeline: false, // 时间线页面开关
 		albums: false, // 相册页面开关
 		devices: false, // 设备页面开关
 	},
